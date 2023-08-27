c061819cc53626b0aac4c6efc3b2238ae205ac0f
Narendranath Chakraborty <nchakraborty195@gmail.com>
2023-08-27 23:57:05 +0530

    changes done in farmer dashboard

--- client/src/components/FarmersDashboard.js
+++ client/src/components/FarmersDashboard.js
@@ -1,7 +1,55 @@
+// import React, { useState, useEffect } from 'react';
+// import './FarmersDashboard.css';
+
+// const server_uri = "http://localhost:4000";
+
+// function FarmersDashboard() {
+//   const [warehouses, setWarehouses] = useState([]);
+
+//   useEffect(() => {
+//     fetchWarehouses();
+//   }, []);
+
+  
+//   const fetchWarehouses = async () => {
+//     try {
+//       const response = await fetch('http://localhost:4000/api/warehouses/fetch/');
+//       if (response.ok) {
+//         const data = await response.json();
+//         console.log('Fetched data:', data);
+//         setWarehouses(data);
+//       } else {
+//         console.error('Response not OK:', response.status);
+//       }
+//     } catch (error) {
+//       console.error('Error fetching warehouses:', error);
+//     }
+//   };
+  
+
+//   return (
+//     <div className="farmers-dashboard">
+//       <h2>Farmers Dashboard</h2>
+//       <div className="warehouse-cards">
+//         {warehouses.map((warehouse) => (
+//           <div key={warehouse._id} className="warehouse-card">
+//             <h3>{warehouse.name}</h3>
+//             <p>{warehouse.description}</p>
+//             <p>Size: {warehouse.size}</p>
+//             <p>Location: {warehouse.location}</p>
+//             {warehouse.image && (
+//               <img src={`data:image/jpeg;base64,${warehouse.image}`} alt={warehouse.name} />
+//             )}
+//           </div>
+//         ))}
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default FarmersDashboard;
 import React, { useState, useEffect } from 'react';
 import './FarmersDashboard.css';
-
-const server_uri = "http://localhost:4000";
 
 function FarmersDashboard() {
   const [warehouses, setWarehouses] = useState([]);
@@ -10,13 +58,11 @@ function FarmersDashboard() {
     fetchWarehouses();
   }, []);
 
-  
   const fetchWarehouses = async () => {
     try {
       const response = await fetch('http://localhost:4000/api/warehouses/fetch/');
       if (response.ok) {
         const data = await response.json();
-        console.log('Fetched data:', data);
         setWarehouses(data);
       } else {
         console.error('Response not OK:', response.status);
@@ -25,20 +71,30 @@ function FarmersDashboard() {
       console.error('Error fetching warehouses:', error);
     }
   };
-  
 
   return (
     <div className="farmers-dashboard">
-      <h2>Farmers Dashboard</h2>
+      <nav className="navbar">
+        <div className="navbar-container">
+          <a href="/" className="navbar-logo">Farmers Portal</a>
+          <ul className="navbar-menu">
+            <li className="navbar-item"><a href="/" className="navbar-link">Home</a></li>
+            <li className="navbar-item"><a href="/" className="navbar-link">About</a></li>
+            <li className="navbar-item"><a href="/" className="navbar-link">Services</a></li>
+            <li className="navbar-item"><a href="/" className="navbar-link">Contact</a></li>
+          </ul>
+        </div>
+      </nav>
+
       <div className="warehouse-cards">
         {warehouses.map((warehouse) => (
-          <div key={warehouse._id} className="warehouse-card">
+          <div className="warehouse-card" key={warehouse._id}>
             <h3>{warehouse.name}</h3>
             <p>{warehouse.description}</p>
             <p>Size: {warehouse.size}</p>
             <p>Location: {warehouse.location}</p>
             {warehouse.image && (
-              <img src={`data:image/jpeg;base64,${warehouse.image}`} alt={warehouse.name} />
+              <img src={`data:image/jpeg;base64,base64ImageDataHere,${warehouse.image}`} alt={warehouse.name} />
             )}
           </div>
         ))}
